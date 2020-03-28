c6490db9eec4d11701227b33d7e7e7a72f8cf4ef
kean <keanli1996@foxmail.com>
2020-03-28 14:37:12 +0800

    some style change

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,14 +81,14 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null){
+        if (A == null) {
             return B;
         }
-        if (B == null){
+        if (B == null) {
             return A;
         }
         var q = A;
-        while (q.rest != null){
+        while (q.rest != null) {
             q = q.rest;
         }
         q.rest = B;
@@ -100,7 +100,7 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        if (A == null){
+        if (A == null) {
             return B;
         }
         return new IntList(A.first, catenate(A.rest, B));
